bce7955c87a15955affb8b67782af1b25c56d259
zyh9 <1124596031@qq.com>
2018-04-10 11:52:24 +0800

    hui

--- react-webpack/app/component/index/Index.jsx
+++ react-webpack/app/component/index/Index.jsx
@@ -1,10 +1,12 @@
 import React,{Component} from 'react';
+import '../../public/css/index.pcss';
 
 class Index extends Component{
     render(){
         return(
             <div className="index">
-                111
+                <h3>111</h3>
+                <div className="con"></div>
             </div>
         )
     }
