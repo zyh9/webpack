236ca5bd69e2f0db017c610d5677d5bffb6c51dd
zyh9 <1124596031@qq.com>
2018-04-13 16:50:15 +0800

    hui

--- react-webpack4/config/webpack/webpack.file.conf.js
+++ react-webpack4/config/webpack/webpack.file.conf.js
@@ -2,5 +2,5 @@ module.exports = {
     devDirectory: 'devBuild',/*开发目录*/
     proDirectory: 'dist',/*发布目录*/
     resource: 'resource',/*静态资源存放目录*/
-    resourcePrefix: 'resource', /*静态资源前缀*/
+    resourcePrefix: 'resource', /*线上静态资源前缀*/
 };
