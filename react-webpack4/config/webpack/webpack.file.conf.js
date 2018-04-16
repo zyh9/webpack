71ea3bbf81ab9b0c622a7d074ff4fe179a7a56ea
zyh9 <1124596031@qq.com>
2018-04-16 19:11:28 +0800

    hui

--- react-webpack4/config/webpack/webpack.file.conf.js
+++ react-webpack4/config/webpack/webpack.file.conf.js
@@ -1,6 +1,6 @@
 module.exports = {
     devDirectory: 'devBuild',/*开发目录*/
-    proDirectory: 'build',/*发布目录*/
+    proDirectory: 'dist',/*发布目录*/
     resource: 'resource',/*静态资源存放目录*/
     resourcePrefix: 'resource',/*线上静态资源前缀*/
     projectName:'react-demo',/*压缩文件名称*/
