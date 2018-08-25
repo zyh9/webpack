9a9eaf457d0f0193747bf10e57e2a6d6c3354a68
zyh9 <1124596031@qq.com>
2018-08-25 15:52:16 +0800

    更新npm包

--- entryBuild/demo.js
+++
@@ -1,4 +0,0 @@
-import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import Index from '../src/page/demo/Index.jsx';
-ReactDOM.render([<Index key="Index"/>],document.getElementById('app'));
--- entryBuild/shop.js
+++
@@ -1,4 +0,0 @@
-import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import Index from '../src/page/shop/Index.jsx';
-ReactDOM.render([<Index key="Index"/>],document.getElementById('app'));
